f49ef6650fcf0cd4d58d2d4bd9993c8146a8922d
lamnguyengpfda <slamnguyengp@gmail.com>
2023-09-01 15:42:13 +0700

    hrllo

---
+++ src/LinkedlistDataStruct/addElementIntoLinkedList.java
@@ -0,0 +1,8 @@
+package LinkedlistDataStruct;
+
+public class addElementIntoLinkedList {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        lis
+    }
+}
