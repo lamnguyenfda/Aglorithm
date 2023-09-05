20f1424a3a5f740403e993f04ab7fc223aaa5d86
lamnguyengpfda <slamnguyengp@gmail.com>
2023-09-05 16:19:10 +0700

    hello

---
+++ src/SearchAglorithm/LinearSearch.java
@@ -0,0 +1,30 @@
+package SearchAglorithm;
+
+import java.util.Scanner;
+
+public class LinearSearch {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int [] a = new int[n];
+        for (int i : a) {
+            i = sc.nextInt();
+        }
+      int x = sc.nextInt();
+      System.out.println(search(a, x, n));
+      
+    }
+
+    static int search(int []arr, int x , int length ){
+           for (int i = 0; i < length ; i++) {
+            if (arr[i] == x) {
+                return i;
+            }else{
+               return -1;
+            }
+           }
+       
+    }
+}
+
+
